b89bc6c4d9b6d3ab6e5dfd1f2873f7adef103974
Ellis Johnson <elljohns@redhat.com>
2021-06-29 09:41:26 +1000

    Cluster list page changes - icons, buttons

--- pkg/portal/cluster.go
+++ pkg/portal/cluster.go
@@ -18,12 +18,18 @@ import (
 )
 
 type AdminOpenShiftCluster struct {
-	Key          string `json:"key"`
-	Name         string `json:"name"`
-	Id           string `json:"id"`
-	State        string `json:"state2"`
-	FailedState  string `json:"failedState"`
-	Subscription string `json:"subscription"`
+	Key           string `json:"key"`
+	Name          string `json:"name"`
+	Id            string `json:"id"`
+	State         string `json:"state"`
+	Version       string `json:"version"`
+	CreatedDate   string `json:"createdDate"`
+	LastModified  string `json:"lastModified"`
+	ProvisionedBy string `json:"provisionedBy"`
+	FailedState   string `json:"failedState"`
+	Subscription  string `json:"subscription"`
+	ResourceGroup string `json:"resourceGroup"`
+	ConsoleLink   string `json:"consoleLink"`
 }
 
 func (p *portal) clusters(w http.ResponseWriter, r *http.Request) {
@@ -39,21 +45,37 @@ func (p *portal) clusters(w http.ResponseWriter, r *http.Request) {
 
 	re := regexp.MustCompile(`(?m)^/subscriptions/([^/]*)/resourceGroups/([^/]*)/providers/Microsoft.RedHatOpenShift/openShiftClusters/(.*)`)
 	for _, doc := range docs.OpenShiftClusterDocuments {
+		if doc.OpenShiftCluster == nil {
+			continue
+		}
+
 		ps := doc.OpenShiftCluster.Properties.ProvisioningState
 		fps := doc.OpenShiftCluster.Properties.FailedProvisioningState
 		subscription := "Unknown"
+		resourceGroup := "Unknown"
 		name := "Unknown"
 		if m := re.FindAllStringSubmatch(doc.OpenShiftCluster.ID, -1); len(m) == 1 && len(m[0]) == 4 {
 			subscription = m[0][1]
+			resourceGroup = m[0][2]
 			name = m[0][3]
 		}
+		LastModified := "Unknown"
+		if doc.OpenShiftCluster.SystemData.LastModifiedAt != nil {
+			LastModified = doc.OpenShiftCluster.SystemData.LastModifiedAt.String()
+		}
+
 		clusters = append(clusters, &AdminOpenShiftCluster{
-			Key:          doc.ID,
-			Id:           doc.OpenShiftCluster.ID,
-			Name:         name,
-			Subscription: subscription,
-			State:        ps.String(),
-			FailedState:  fps.String(),
+			Key:           doc.ID,
+			Id:            doc.OpenShiftCluster.ID,
+			Name:          name,
+			Subscription:  subscription,
+			ResourceGroup: resourceGroup,
+			Version:       doc.OpenShiftCluster.Properties.ClusterProfile.Version,
+			CreatedDate:   doc.OpenShiftCluster.Properties.CreatedAt.String(),
+			LastModified:  LastModified,
+			ProvisionedBy: doc.OpenShiftCluster.Properties.ProvisionedBy,
+			State:         ps.String(),
+			FailedState:   fps.String(),
 		})
 	}
 
